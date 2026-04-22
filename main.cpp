54696749903129d403ba4f8ffb92b0559a1fd279
snanousaid <s.snanou@nextronic.io>
2026-04-22 17:22:51 +0100

    remove QT_QPA_PLATFORM/EGLFS_INTEGRATION from code and env config

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-    qputenv("QT_QPA_PLATFORM", "eglfs");
-    qputenv("QT_QPA_EGLFS_INTEGRATION", "none");
-
 #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
